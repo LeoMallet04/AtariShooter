6bae202a8989a469c4fa946a149663a177feafc4
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-07 18:16:55 -0300

    Criação da lógica inicial do jogo

--- AtariShooter.go
+++ AtariShooter.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt";
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+)
 
 func main(){
-	fmt.Print("Hello World!")
+	//Cria nova tela
+	screen, err := tcell.NewScreen()
+
+	//Trata o erro
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	//Adia o comportamento de limpar a tela como ultima coisa
+	defer screen.Fini();
+
+	//Inicializa a tela criada 
+	err = screen.Init();
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//game init
+	player := NewSprite('@',10,10)
+
+	//Loop principal do jogo
+	running := true 
+	for running {
+
+		//game init
+
+		screen.Clear()
+
+		player.Draw(screen)
+		
+		screen.Show()
+
+	
+
 }
