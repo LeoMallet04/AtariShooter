b2e7e546d53856a809e5dd7a0717b44917725517
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-29 19:39:32 -0300

    Correções na movimentação dos sprites

--- AtariShooter.go
+++ AtariShooter.go
@@ -206,58 +206,59 @@ func initPlayers(quantPlayer int, playerC chan Sprite){
 }
 
 
-func moveSprites(ev tcell.Event,players[]Sprite,dirs[]rune, bullets *[]*Bullet, running *bool){
+func moveSprites(ev tcell.Event, localState *GameState,dirs[]rune, running *bool){
 	switch ev := ev.(type) {
 	case *tcell.EventKey:
 	switch ev.Key() {
 		// Player 2 - setas
 		case tcell.KeyUp:
 			dirs[1] = 'w'
-			players[1].Move('w')
+			localState.Players[1].Move('w')
 		case tcell.KeyDown:
 			dirs[1] = 's'
-			players[1].Move('s')
+			localState.Players[1].Move('s')
 		case tcell.KeyLeft:
 			dirs[1] = 'a'
-			players[1].Move('a')
+			localState.Players[1].Move('a')
 		case tcell.KeyRight:
 			dirs[1] = 'd'
-			players[1].Move('d')
+			localState.Players[1].Move('d')
 		case tcell.KeyEnter:
 			var bx, by int
 			switch dirs[1] {
 			case 'w':
-				bx, by = players[1].X, players[1].Y-1
+				bx, by = localState.Players[1].X, localState.Players[1].Y-1
 			case 'a':
-				bx, by = players[1].X-1, players[1].Y
+				bx, by = localState.Players[1].X-1, localState.Players[1].Y
 			case 's':
-				bx, by = players[1].X, players[1].Y+1
+				bx, by = localState.Players[1].X, localState.Players[1].Y+1
 			case 'd':
-				bx, by = players[1].X+1, players[1].Y
+				bx, by = localState.Players[1].X+1, localState.Players[1].Y
 			}
 			bullet := NewBullet(bx, by, dirs[1])
 			bullet.Char = '*' // Diferencia os tiros do player 2
-			*bullets = append(*bullets, bullet)
+			localState.Bullets = append(localState.Bullets, bullet)
 
 		// Player 1 - wasd + e para atirar
 		case tcell.KeyRune:
 			switch ev.Rune() {
 			case 'w', 'a', 's', 'd':
 				dirs[0] = ev.Rune()
-				players[0].Move(ev.Rune())
+				localState.Players[0].Move(ev.Rune())
 			case 'e':
 				var bx, by int
 				switch dirs[0] {
 				case 'w':
-					bx, by = players[0].X, players[0].Y-1
+					bx, by = localState.Players[0].X, localState.Players[0].Y-1
 				case 'a':
-					bx, by = players[0].X-1, players[0].Y
+					bx, by = localState.Players[0].X-1, localState.Players[0].Y
 				case 's':
-					bx, by = players[0].X, players[0].Y+1
+					bx, by = localState.Players[0].X, localState.Players[0].Y+1
 				case 'd':
-					bx, by = players[0].X+1, players[0].Y
+					bx, by = localState.Players[0].X+1, localState.Players[0].Y
 				}
-				*bullets = append(*bullets, NewBullet(bx, by, dirs[0]))
+				bullet := NewBullet(bx,by,dirs[0])
+				localState.Bullets = append(localState.Bullets, bullet)
 			case 'q':
 				*running = false
 			}
