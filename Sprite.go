fe93dcdc71746581fd5ea1815d2bf903b017f4dc
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-29 17:50:14 -0300

    Implementação das conversões de Sprites para mensagens e vice-versa

--- Sprite.go
+++ Sprite.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/gdamore/tcell/v2"
 )
 
@@ -38,4 +42,28 @@ func (s *Sprite) Move(direction rune) {
 	case 'd':
 		s.X += 1
 	}
+}
+
+func SpriteToString(s *Sprite) string{
+	return fmt.Sprintf("%c;%d,%d",s.Char, s.X, s.Y)
+}
+
+func StringToSprite(str string) (*Sprite, error){
+	parts := strings.Split(str, ";")
+	if len(parts) != 4 {
+		return nil, fmt.Errorf("ERRO: conversao de volta para a bala, número de args: %s", str)
+	}
+	char := []rune(parts[0])
+	if len(char) != 1{
+		return nil, fmt.Errorf("ERRO: conversão de volta para a bala, Char: %c",char)
+	}
+	x, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return nil, fmt.Errorf("ERRO: conversao de volta para a bala, X: %d", x)
+	}
+	y, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return nil, fmt.Errorf("ERRO: conversao de volta para a bala, Y: %d", y)
+	}
+	return &Sprite{Char: char[0], X: x, Y: y}, nil
 }
